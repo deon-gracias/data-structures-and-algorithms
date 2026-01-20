15153825d5a0b621c7ad5151bec62debb1712b42
deon-gracias <deongracias1@gmail.com>
2026-01-20 23:15:41 +0530

    Binary search tree in java with add, print and search

--- DataStructures/BinarySearchTree.java
+++ DataStructures/BinarySearchTree.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class BinarySearchTree {
-  public static class Node{
+  public static class Node {
     public int value;
     public Node left;
     public Node right;
@@ -11,7 +11,6 @@ public class BinarySearchTree {
       this.left = null;
       this.right = null;
     }
-
 
     public Node(Node left, Node right) {
       this.left = left;
@@ -25,52 +24,81 @@ public class BinarySearchTree {
     }
   }
 
-  public static Node add(Node root, Node new) {
+  public static Node add(Node root, Node n) {
     if (root == null) {
-      return new;
+      return n;
     }
 
     Node p = root;
 
     while (p != null) {
-      if (p.value > new.value) {
+      if (p.value > n.value) {
         if (p.left == null) {
-          p.left = new;
-          return root
+          p.left = n;
+          return root;
         }
         p = p.left;
       }
 
-      if (p.value < new.value) {
+      if (p.value < n.value) {
         if (p.right == null) {
-          p.right = new;
-          return root
+          p.right = n;
+          return root;
         }
         p = p.right;
       }
     }
 
-    return root
+    return root;
+  }
+
+  public static int search(Node root, int value) {
+    Node p = root;
+    int level = 0;
+    while (p != null) {
+      // System.out.printf("%d - %d\n", p.value, value);
+
+      if (p.value == value)
+        return level;
+
+      if (p.value > value)
+        p = p.left;
+
+      if (p.value < value)
+        p = p.right;
+
+      level++;
+    }
+
+    return -1;
   }
 
   public static void print(Node root) {
     if (root == null) {
-      return
+      return;
     }
 
-    System.out.printf(root.value);
-    print(root->left);
-    print(root->right);
+    System.out.printf("%d ", root.value);
+    print(root.left);
+    print(root.right);
   }
 
   public static void main() {
     Node root = null;
-    add(root, new Node(5));
-    add(root, new Node(3));
-    add(root, new Node(2));
-    add(root, new Node(8));
-    add(root, new Node(7));
+    root = add(root, new Node(3));
+    root = add(root, new Node(2));
+    root = add(root, new Node(5));
+    root = add(root, new Node(8));
+    root = add(root, new Node(9));
+    root = add(root, new Node(7));
+
+    int level = search(root, 8);
+    if (level >= 0)
+      System.out.printf("Found element at %d\n", level);
+    else
+      System.out.println("Couldn't find element ");
 
     print(root);
+    System.out.println();
   }
 }
